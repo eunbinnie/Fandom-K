c93912e6fdbd58dd1e648f2b4a4610a8a2176c08
Sombian <sombian.software@gmail.com>
2024-04-30 08:54:23 +0900

    feat: workspace settings

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+export default
+{
+	semi: true,
+	useTabs: true,
+	singleQuote: false,
+	trailingComma: "all",
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from "path"; import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-});
+/** @type {import("vite").UserConfig} */
+export default
+{
+	plugins:
+	[
+		react()
+	],
+	resolve:
+	{
+		alias:
+		[
+			// eslint-disable-next-line no-undef
+			{ find: "@", replacement: path.resolve(__dirname, "src") }
+		]
+	}
+};
